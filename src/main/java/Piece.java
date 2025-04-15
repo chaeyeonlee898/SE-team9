207921c847b12d5d186c3db713eac1178f957aad
jihuuuu <jihuu2806@gmail.com>
2025-04-15 16:40:56 +0900

    FEAT : GIT 초기 환경 설정

---
+++ src/main/java/Piece.java
@@ -0,0 +1,19 @@
+//Piece.java
+class Piece {
+    Player owner;
+    BoardNode position;  // 처음에는 null (집에 있음)
+    boolean finished;
+
+    public Piece(Player owner) {
+        this.owner = owner;
+        this.finished = false;
+        this.position = null; // 초기 상태: 집 (보드에 없음)
+    }
+
+    @Override
+    public String toString() {
+        String info = "말@" + (position != null ? position.name : "집");
+        if (finished) info += "(완주)";
+        return info;
+    }
+}
