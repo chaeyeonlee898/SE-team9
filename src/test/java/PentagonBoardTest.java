f74bcab47587ff2050e8692f8ff19757f09dac64
jihuuuu <jihuu2806@gmail.com>
2025-05-10 23:34:28 +0900

    ADD : MULTI TEST CASE 추가 및 빽도 오류 수정

--- src/test/java/PentagonBoardTest.java
+++ src/test/java/PentagonBoardTest.java
@@ -3,12 +3,24 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import model.*;
+@DisplayName("PentagonBoard 교차점별 이동 로직 테스트")
 public class PentagonBoardTest extends AbstractBoardTest {
 
     @Override
     protected Board createBoard() {
         return new PentagonBoard();
+    }
+
+    @Override
+    protected int numberOfPlayers() {
+        return 1;
+    }
+
+    @Override
+    protected int piecesPerPlayer() {
+        return 1;
     }
 
     /** 0(모)->5(걸)->35(윷)->0(완주) + 윷 이후 완주일 때 추가 윷 던지기 발생 X 확인 */
@@ -19,7 +31,7 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.GEOL.getStepCount(),
                 YutResult.YUT.getStepCount()
         );
-        assertTrue(piece.isFinished());
+        assertTrue(piece(0,0).isFinished());
     }
 
     /** 0(모)->5(모)->32(윷)->23(걸)->0(완주) */
@@ -30,9 +42,9 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.MO.getStepCount(),
                 YutResult.YUT.getStepCount()
         );
-        assertEquals(23, piece.getPosition().getId(), "말의 위치는 23 이어야 합니다.");
+        assertEquals(23, piece(0,0).getPosition().getId(), "말의 위치는 23 이어야 합니다.");
         apply(YutResult.GEOL.getStepCount());
-        assertTrue(piece.isFinished());
+        assertTrue(piece(0,0).isFinished());
     }
 
     /** 0(윷)->4(모)->9(도)->10(걸)->35(걸)->0(빽도)->34(미완주) */
@@ -46,8 +58,8 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.GEOL.getStepCount(),
                 YutResult.BACKDO.getStepCount()
         );
-        assertEquals(34, piece.getPosition().getId(), "말의 위치는 34 이어야 합니다.");
-        assertFalse(piece.isFinished());
+        assertEquals(34, piece(0,0).getPosition().getId(), "말의 위치는 34 이어야 합니다.");
+        assertFalse(piece(0,0).isFinished());
     }
 
     /** 0(윷)->4(모)->9(도)->10(모)->32(모)->24(도)->0(빽도)->24(미완주) */
@@ -62,8 +74,8 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.DO.getStepCount(),
                 YutResult.BACKDO.getStepCount()
         );
-        assertEquals(24, piece.getPosition().getId(), "말의 위치는 24 이어야 합니다.");
-        assertFalse(piece.isFinished());
+        assertEquals(24, piece(0,0).getPosition().getId(), "말의 위치는 24 이어야 합니다.");
+        assertFalse(piece(0,0).isFinished());
     }
 
     /** 0(윷)->4(모)->9(모)->14(도)->15(걸)->35(걸)->0(개)->0(완주) */
@@ -78,9 +90,9 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.GEOL.getStepCount(),
                 YutResult.GEOL.getStepCount()
         );
-        assertEquals(0, piece.getPosition().getId(), "말의 위치는 0 이어야 합니다.");
+        assertEquals(0, piece(0,0).getPosition().getId(), "말의 위치는 0 이어야 합니다.");
         apply(YutResult.GAE.getStepCount());
-        assertTrue(piece.isFinished());
+        assertTrue(piece(0,0).isFinished());
     }
 
     /** 0(윷)->4(모)->9(모)->14(도)->15(모)->32(모)->24(개)->0(완주) */
@@ -94,10 +106,10 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.MO.getStepCount(),
                 YutResult.MO.getStepCount()
         );
-        assertEquals(24, piece.getPosition().getId(), "말의 위치는 24 이어야 합니다.");
+        assertEquals(24, piece(0,0).getPosition().getId(), "말의 위치는 24 이어야 합니다.");
         apply(YutResult.GAE.getStepCount());
 
-        assertTrue(piece.isFinished());
+        assertTrue(piece(0,0).isFinished());
     }
 
     /** 0(윷)->4(모)->9(모)->14(윷)->18(개)->20(모)->0(미완주) */
@@ -111,7 +123,7 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.GAE.getStepCount(),
                 YutResult.MO.getStepCount()
         );
-        assertFalse(piece.isFinished());
+        assertFalse(piece(0,0).isFinished());
     }
 
     /** 0(윷)->4(모)->9(모)->14(윷)->18(모)->23(걸)->0(완주) */
@@ -125,7 +137,7 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.MO.getStepCount(),
                 YutResult.GEOL.getStepCount()
         );
-        assertTrue(piece.isFinished());
+        assertTrue(piece(0,0).isFinished());
     }
 
     /** e. 0(도)->1(빽도)->0(빽도)->1(빽도)->0(개)->0(완주) */
@@ -136,12 +148,12 @@ public class PentagonBoardTest extends AbstractBoardTest {
                 YutResult.DO.getStepCount(),
                 YutResult.BACKDO.getStepCount()
         );
-        assertEquals(0, piece.getPosition().getId(),"위치는 0 이어야 합니다.");
+        assertEquals(0, piece(0,0).getPosition().getId(),"위치는 0 이어야 합니다.");
         apply(YutResult.BACKDO.getStepCount());
-        assertEquals(1, piece.getPosition().getId(),"위치는 1 이어야 합니다.");
+        assertEquals(1, piece(0,0).getPosition().getId(),"위치는 1 이어야 합니다.");
         apply(YutResult.BACKDO.getStepCount());
-        assertEquals(0, piece.getPosition().getId(),"위치는 0 이어야 합니다.");
+        assertEquals(0, piece(0,0).getPosition().getId(),"위치는 0 이어야 합니다.");
         apply(YutResult.GAE.getStepCount());
-        assertTrue(piece.isFinished(), "말이 완주되어야 합니다.");
+        assertTrue(piece(0,0).isFinished(), "말이 완주되어야 합니다.");
     }
 }
